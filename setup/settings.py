3a203fcc1f1a08f1e99735067417e45886fa68c0
igornardoni <igornardonik@gmail.com>
2023-10-24 18:54:47 -0300

    add content

--- setup/settings.py
+++ setup/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'watchflix',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from watchflix.views import ProgramaViewSet
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="WatchFlix DB - API",
+      default_version='v1',
+      description="Movie and TV Show Database. "
+                  "Developed by Igor Nardoni, from Nardoni Tech",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 router = routers.DefaultRouter()
 router.register('programas', ProgramaViewSet, basename='programas')
@@ -9,4 +29,7 @@ router.register('programas', ProgramaViewSet, basename='programas')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path(
+        'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'
+    ),
 ]
