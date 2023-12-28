5a7f7db3eb77f34b936c7b29c3e40d760af4df4f
igornardoni <igornardonik@gmail.com>
2023-12-28 10:17:51 -0300

    fix

--- watchflix/models.py
+++ watchflix/models.py
@@ -5,7 +5,8 @@ class Programa(models.Model):
     TIPO = (('F', 'Filme'), ('S', 'Serie'),)
 
     titulo = models.CharField(max_length=50)
-    tipo = models.CharField(max_length=1, choices=TIPO, blank=False, null=False, default='F')
+    tipo = models.CharField(max_length=1, choices=TIPO, blank=False, 
+                            null=False, default='F')
     data_lancamento = models.DateField()
     likes = models.PositiveIntegerField(default=0)
     dislikes = models.PositiveIntegerField(default=0)
